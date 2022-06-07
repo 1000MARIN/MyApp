de29f69ef304d745f4a876085c3c22d225d6b867
1000marin <leexjin@gmail.com>
2022-06-08 08:53:01 +0900

    CDT_인지기능검사_Android

---
+++ app/src/main/java/com/example/myapp/MainActivity2.java
@@ -0,0 +1,27 @@
+package com.example.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        //시작 버튼 클릭시 액티비티 전환
+        Button next_btn_1 = (Button) findViewById(R.id.next_btn_1);
+        next_btn_1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(), MainActivity3.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
